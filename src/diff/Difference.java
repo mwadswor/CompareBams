8371984930c840ef5573409509a5d1f3a33ec47a
Mark Ebbert <me.mark@gmail.com>
2014-12-12 11:21:49 -0700

    Made several updates.

--- src/diff/Difference.java
+++ src/diff/Difference.java
@@ -13,6 +13,7 @@ import htsjdk.samtools.util.IOUtil;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -26,6 +27,7 @@ public class Difference {
 //	private Set<String> readNames;
 	private SAMFileHeader header;
 	private File outfile;
+	private HashMap<String, Integer> chromLengths;
 	
 	public Difference(File infile1, File infile2, File output) {
 //		this.differences = new ArrayList<SAMRecord>();
@@ -37,8 +39,35 @@ public class Difference {
 		IOUtil.assertFileIsReadable(infile1);
 		IOUtil.assertFileIsReadable(infile2);
 		
-		runDifferenceCheck(infile1,infile2);
+		chromLengths = new HashMap<String, Integer>();
+		chromLengths.put("chrM", 16571);
+		chromLengths.put("chr1", 249250621);
+		chromLengths.put("chr2", 243199373);
+		chromLengths.put("chr3", 198022430);
+		chromLengths.put("chr4", 191154276);
+		chromLengths.put("chr5", 180915260);
+		chromLengths.put("chr6", 171115067);
+		chromLengths.put("chr7", 159138663);
+		chromLengths.put("chr8", 146364022);
+		chromLengths.put("chr9", 141213431);
+		chromLengths.put("chr10", 135534747);
+		chromLengths.put("chr11", 135006516);
+		chromLengths.put("chr12", 133851895);
+		chromLengths.put("chr13", 115169878);
+		chromLengths.put("chr14", 107349540);
+		chromLengths.put("chr15", 102531392);
+		chromLengths.put("chr16", 90354753);
+		chromLengths.put("chr17", 81195210);
+		chromLengths.put("chr18", 78077248);
+		chromLengths.put("chr19", 59128983);
+		chromLengths.put("chr20", 63025520);
+		chromLengths.put("chr21", 48129895);
+		chromLengths.put("chr22", 51304566);
+		chromLengths.put("chrX", 155270560);
+		chromLengths.put("chrY", 59373566);
+		chromLengths.put("*", 0);
 
+		runDifferenceCheck(infile1,infile2);
 	}
 	
 	private void runDifferenceCheck(File infile1, File infile2){
@@ -51,42 +80,57 @@ public class Difference {
         
 		header = headerAndIterator2.header;
 //		SAMRecord prevRec = new SAMRecord(header);
-		final SAMFileWriter out = new SAMFileWriterFactory().makeSAMOrBAMWriter(header, false, outfile);
+		final SAMFileWriter out = new SAMFileWriterFactory().makeSAMOrBAMWriter(header, true, outfile);
 		
 //		THIRD ROUND********************************************************		
 		
 		String Chromosome = "";
-		String tempCHR = "";
+		String tempCHR = "", temp2CHR;
+		int chromEnd = -1;
+		int binEnd = -1;
+		int binSize = 100000;
+		int currPos = -1, currPos2 = -1;
 		SAMRecord rec1 = iterator1.next();
 		SAMRecord rec2 = iterator2.next();
+		int iterCount = 0;
 		
+		Chromosome = rec1.getReferenceName();
+		tempCHR = rec1.getReferenceName();
+		chromEnd = this.chromLengths.get(Chromosome);
+		binEnd = binSize;
+		currPos = rec1.getAlignmentStart();
 		while(iterator1.hasNext() && iterator2.hasNext()){
 			
 			HashSet<String> smallerFile = new HashSet<String>();
 			
-			while(Chromosome.equals(tempCHR)){
+			if(iterCount % 100 == 0){
+				reportMemoryStats("Iteration " + iterCount + ": ");
+				System.out.println("Chrom: " + Chromosome + ", CurrPos: " + currPos + ", chromEnd: " + chromEnd + "\n");
+			}
+			
+			while(Chromosome.equals(tempCHR) && currPos <= binEnd){
 				
-				
-				if(Chromosome.equals("")){
-					Chromosome = rec1.getReferenceName();
+				/* Stop when we hit chr '*' */
+				if(!tempCHR.equals("*")){
+					smallerFile.add(makeStringFromRec(rec1));
 				}
-				
-				tempCHR = rec1.getReferenceName();
-				smallerFile.add(makeStringFromRec(rec1));
+
 				
 				if(!iterator1.hasNext())
 					break;
 				
 				rec1 = iterator1.next();
 				tempCHR = rec1.getReferenceName();
+				currPos = rec1.getAlignmentStart();
 			}
 			
-			String temp2CHR = Chromosome;
-			while(Chromosome.equals(temp2CHR)){
+			//String temp2CHR = Chromosome;
+			temp2CHR = rec2.getReferenceName();
+			currPos2 = rec2.getAlignmentStart();
+			while(Chromosome.equals(temp2CHR) && currPos2 <= binEnd){
 				
-				temp2CHR = rec2.getReferenceName();
-				
-				if(!smallerFile.contains(makeStringFromRec(rec2))){
+				if(!temp2CHR.equals("*")
+						&& !smallerFile.contains(makeStringFromRec(rec2))){
 					out.addAlignment(rec2);
 				}
 				if(!iterator2.hasNext())
@@ -94,17 +138,39 @@ public class Difference {
 				
 				rec2 = iterator2.next();
 				temp2CHR = rec2.getReferenceName();
+				currPos2 = rec2.getAlignmentStart();
 			}
 			
-			System.out.println(tempCHR + " == " + temp2CHR);
+//			System.out.println(tempCHR + " == " + temp2CHR);
+//			System.out.println("CurrPos: " + currPos + ", binEnd: " + binEnd + ", chromEnd: " + chromEnd);
 			if(!tempCHR.equals(temp2CHR)){
-				System.out.println("You're file is not sorted based on Chromosome and position.");
+				System.out.println("Your file is not sorted based on Chromosome and position.");
 				System.exit(1);
 			}
-			System.out.println(Chromosome);
-			Chromosome = tempCHR;
+//			System.out.println(Chromosome);
 			
+			/* If tempCHR has moved to the next chr, reset
+			 * the currPos, chromEnd, and binEnd
+			 */
+			if(!Chromosome.equals(tempCHR)){
+				System.out.println("Starting chrom: '" + tempCHR + "'");
+				Chromosome = tempCHR;
+				chromEnd = this.chromLengths.get(Chromosome);
+				binEnd = binSize;
+			}
+			/* Otherwise, if the remaining portion of the chr is
+			 * less than the bin size, just set binEnd to the chromEnd
+			 * so we don't go over.
+			 */
+			else if(chromEnd - binSize < binEnd){
+				binEnd = chromEnd;
+			}
+			/* Otherwise, just add binSize to binEnd */
+			else{
+				binEnd += binSize;
+			}
 			
+			iterCount++;
 		}
 		
 		
@@ -182,9 +248,18 @@ public class Difference {
 	
 	private String makeStringFromRec(SAMRecord rec){
 		StringBuilder line = new StringBuilder();
-		line.append(rec.getReadName()+"\t"+rec.getReadString());
+		line.append(rec.getReadName()+"\t"+rec.getFlags()+"\t"+rec.getReadString());
 		return line.toString();
 	}
+	
+    /** Print out some quick JVM memory stats. */
+    private void reportMemoryStats(final String stage) {
+        System.gc();
+        final Runtime runtime = Runtime.getRuntime();
+        System.out.println(stage + " freeMemory: " + runtime.freeMemory() + "; totalMemory: " + runtime.totalMemory() +
+                "; maxMemory: " + runtime.maxMemory());
+    }
+    
 	
 //	private void checkNextRead(CloseableIterator<SAMRecord> iterator1, CloseableIterator<SAMRecord> iterator2, SAMRecord rec1, SAMRecord rec2) {
 //		SAMRecord prevRec1 = rec1;
